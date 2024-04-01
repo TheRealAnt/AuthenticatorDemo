da1169d473b914b2821fa10eb02aeb91cefd5fdc
AntonieSander <antonies9@hotmail.com>
2024-04-01 20:32:18 +0200

    Initial Commit

---
+++ AuthenticatorDemo/AuthenticatorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticatorDemoApp.swift
+//  AuthenticatorDemo
+//
+//  Created by Antonie Sander on 2024/04/01.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticatorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthenticatorDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AuthenticatorDemo
+//
+//  Created by Antonie Sander on 2024/04/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
